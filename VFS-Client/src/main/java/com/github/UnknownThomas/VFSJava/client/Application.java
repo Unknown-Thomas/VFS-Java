229e86d34dd621b465cdc0a946e6056c87d52fbe
Unknown Thomas <ich.spreche.wieder@gmail.com>
2014-11-09 13:52:48 +0300

    add modules and configure assembly plugin

---
+++ VFS-Client/src/main/java/com/github/UnknownThomas/VFSJava/client/Application.java
@@ -0,0 +1,6 @@
+package com.github.UnknownThomas.VFSJava.client;
+
+public class Application {
+    public static void main(String[] args) {
+    }
+}
---
+++ VFS-Server/src/main/java/com/github/UnknownThomas/VFSJava/server/Application.java
@@ -0,0 +1,6 @@
+package com.github.UnknownThomas.VFSJava.server;
+
+public class Application {
+    public static void main(String[] args) {
+    }
+}
